5c2c7a07ff824591f9ae013916562b63d5d05c58
KhaliRasbajus <kevinsalamanca150920@gmail.com>
2024-09-07 16:30:00 -0500

    Cambios

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { envs } from './config';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: envs.nats_service,
+      },
+    },
+  );
+  await app.listen();
+}
+bootstrap();
