8a0ce38b6791d0100a1ec54f26577c91061dbcfd
KhaliRasbajus <kevinsalamanca150920@gmail.com>
2024-09-07 16:32:00 -0500

    cambios

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -8,27 +8,27 @@ import { UpdateOrderDto } from './dto/update-order.dto';
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
-  @MessagePattern('createOrder')
+  @MessagePattern('order.create')
   create(@Payload() createOrderDto: CreateOrderDto) {
     return this.ordersService.create(createOrderDto);
   }
 
-  @MessagePattern('findAllOrders')
+  @MessagePattern('orders.findAll')
   findAll() {
     return this.ordersService.findAll();
   }
 
-  @MessagePattern('findOneOrder')
+  @MessagePattern('order.findOne')
   findOne(@Payload() id: number) {
     return this.ordersService.findOne(id);
   }
 
-  @MessagePattern('updateOrder')
+  @MessagePattern('order.update')
   update(@Payload() updateOrderDto: UpdateOrderDto) {
     return this.ordersService.update(updateOrderDto.id, updateOrderDto);
   }
 
-  @MessagePattern('removeOrder')
+  @MessagePattern('order.remove')
   remove(@Payload() id: number) {
     return this.ordersService.remove(id);
   }
